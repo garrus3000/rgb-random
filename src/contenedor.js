9d4c6457c80e185dc4560b1d2fce04a2e0a55240
Eduardo Odriozola <eduardocorrientes@gmail.com>
2022-06-18 20:50:09 -0300

    incluyo chalk

--- src/contenedor.js
+++ src/contenedor.js
@@ -1,3 +1,5 @@
+const Chalk = require('chalk');
+
 class Contenedor {
     constructor(cantidad) {
         this.cantidad = cantidad;
@@ -13,23 +15,29 @@ class Contenedor {
 
     getRGB() {
         if (this.incorrecto) {
-            console.log(`\n El valor de la cantidad debe ser mayor a 0 y menor a 6
-            \n Cantidad ingresada: ${this.cantidad}
-            \n No se puede generar un RGB`);
+            console.log(`\n El valor de la ${Chalk.blue("cantidad")} debe ser un número mayor a 0 y menor a 6
+            \n Cantidad ingresada: ${Chalk.red(this.cantidad)}
+            \n Error al generar un: ${Chalk.bgAnsi256(this.generateRGBValue())(' RGB ')}`);
             return "";
         }
         const arr = [];
         for (let i = 0; i < this.cantidad; i++) {
-            const rgb = `rgb(${this.generateRGBValue()}, ${this.generateRGBValue()}, ${this.generateRGBValue()})`;
+            const r = this.generateRGBValue();
+            const g = this.generateRGBValue();
+            const b = this.generateRGBValue();
+            const _rgb = `${r},${g},${b}`;
             const id = i + 1;
             const objeto = {
                 id,
-                rgb,
+                _rgb: r, g, b
             };
+            // const chalked = Chalk.rgb(r, g, b)(`\n RGB ${id}: rgb(${_rgb})`);
+            const chalked = Chalk.bgRgb(r, g, b)(`\n RGB ${id} `)+ Chalk.rgb(r, g, b)(` rgb(${_rgb})`);
+
             arr.push(objeto);
+            console.log(chalked);
         }
-        // return arr;
-        return arr.map((obj) => `\n ${obj.id}. ${obj.rgb}`).join("\n");
+        return "";
     }
 }
 
